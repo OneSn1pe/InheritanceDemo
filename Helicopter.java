d63e4bd3d3bd8576d930edd85f46344fe58884ae
Kaustubh Kislay <you@example.com>
2024-01-25 14:47:00 -0500

    created helicopter class(some error)

---
+++ Helicopter.java
@@ -0,0 +1,26 @@
+public class Helicopter extends Vehicle{
+    private int altitude;
+    private int blades;
+
+
+    public Helicopter(int passangers, int blades, int altitude, String color, String type) {
+        
+        super(passangers, color, type);
+
+        this.altitude = altitude;
+        this.blades = blades;
+    }
+    }
+   
+ 
+    public int getBlades() {
+        return blades;
+    }
+    public int getAltitide() {
+        return altitude;
+    }
+
+    public String getType() {
+        return "helicopter";
+        }
+}
